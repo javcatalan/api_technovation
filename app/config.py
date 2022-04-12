e270137b3829e755cbb5f34d0b27553d9ebfcbee
Javier Catalan <95101202+javcatalan@users.noreply.github.com>
2022-04-11 18:37:08 -0600

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,9 @@
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://rztyptvoqgtnqn:738281c0dba82c00317ce0016fa0c79b2b674db70f768191454851a1e7c9730d@ec2-3-225-79-57.compute-1.amazonaws.com:5432/dtas0t0am2jos'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://tgqwhwxkjkcoxf:75ac21563311df22edc48c1d93d978f9924f5c39b3b5bc61a448ca2b5ba1f4c8@ec2-34-224-226-38.compute-1.amazonaws.com:5432/dnqj7pp29h90c'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    
 
+    
 # Host
 #     ec2-3-225-79-57.compute-1.amazonaws.com
 # Database
